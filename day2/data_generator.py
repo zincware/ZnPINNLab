a992029b2d029749da48f0f0e0e6e62c8626f5c6
Lorenzkraft <meyer_lorenz@web.de>
2023-11-07 14:02:51 +0100

    moved requires grad to the ipynb file

--- day2/data_generator.py
+++ day2/data_generator.py
@@ -18,7 +18,7 @@ class Schrodinger_Boundary(Dataset):
     def __init__(self, num_col_bound = 50):  
         self.num_col_bound = num_col_bound
         num_col_bound_half = math.floor(num_col_bound/2)
-        self.t = torch.tensor(np.squeeze(lhs(1,samples=num_col_bound_half)*math.pi/2), requires_grad=True).float().to(device)
+        self.t = torch.unsqueeze(torch.tensor(np.squeeze(lhs(1,samples=num_col_bound_half)*math.pi/2)).float().to(device),1)
         return
     def __getitem__(self,idx):
         return self.t[idx]
@@ -31,7 +31,7 @@ class Schrodinger_Initial(Dataset):
     def __init__(self, num_h_init = 50):            
         h_func = lambda x : 2 * (1/torch.cosh(x))
         self.num_h_init = num_h_init
-        self.x = torch.tensor((lhs(1,samples=num_h_init)*10 - 5), requires_grad=True).float().to(device)
+        self.x = torch.tensor((lhs(1,samples=num_h_init)*10 - 5)).float().to(device)
         self.h = torch.squeeze(torch.stack((
             torch.tensor(h_func(self.x)).float().to(device),
             torch.zeros((len(self.x),1)).to(device)
@@ -47,8 +47,8 @@ class Schrodinger_Initial(Dataset):
 class Schrodinger(Dataset):
     def __init__(self, num_col_schro = 20000): # returns x,t
         self.num_col_schro = num_col_schro
-        x = torch.tensor(lhs(1,samples=num_col_schro)*10 - 5,requires_grad=True).float().to(device)
-        t = torch.tensor(lhs(1,samples=num_col_schro)*(math.pi/2),requires_grad=True).float().to(device)
+        x = torch.tensor(lhs(1,samples=num_col_schro)*10 - 5).float().to(device)
+        t = torch.tensor(lhs(1,samples=num_col_schro)*(math.pi/2)).float().to(device)
         self.X = torch.squeeze(torch.dstack((x,t))).float()
         return 
     def __getitem__(self,idx):
