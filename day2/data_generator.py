8d9a917ce03446b99d4bd6be79e1ea7f59f6b2d6
Lorenzkraft <meyer_lorenz@web.de>
2023-11-13 14:59:19 +0100

    implemented osc init data, osc PINN loss.

--- day2/data_generator.py
+++ day2/data_generator.py
@@ -11,6 +11,7 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import math 
+from scipy import special
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -30,9 +31,9 @@ class Schrodinger_Boundary(Dataset):
 class Schrodinger_Initial(Dataset):
     def __init__(self, num_h_init = 50):            
         h_func = lambda x : 2 * (1/torch.cosh(x))
-        self.num_h_init = num_h_init
+        # self.num_h_init = num_h_init
         self.x = torch.tensor((lhs(1,samples=num_h_init)*10 - 5)).float().to(device)
-        self.t = torch.zeros((num_h_init,1)).float().to(device)
+        self.t = torch.zeros((num_h_init, 1)).float().to(device)
         self.h = torch.squeeze(torch.stack((
             torch.tensor(h_func(self.x)).float().to(device),
             torch.zeros((len(self.x),1)).to(device)
@@ -44,6 +45,26 @@ class Schrodinger_Initial(Dataset):
         return len(self.x), len(self.t), len(self.h)
     def getall(self):
         return  self.x, self.t, self.h
+    
+class Schrodinger_Initial_Oscillator(Dataset):
+    def __init__(self, num_h_init=50, n=1):
+        self.num_samples = num_h_init
+        psi = lambda x: torch.exp(-x ** 2 / 2) * special.hermite(n, monic=True)(x)
+        self.x = torch.tensor((lhs(1,samples=num_h_init)*10 - 5)).float().to(device)
+        self.t = torch.zeros((num_h_init, 1)).float().to(device)
+        self.psi = torch.squeeze(torch.stack((
+            torch.tensor(psi(self.x)).float().to(device),
+            torch.zeros((len(self.x), 1)).to(device)
+        ),1))
+
+    def __getitem__(self, idx):
+        return self.x[idx], self.t[idx], self.psi[idx]
+    
+    def __len__(self):
+        return len(self.x)
+    
+    def getall(self):
+        return self.x, self.t, self.psi
 
 class Schrodinger(Dataset):
     def __init__(self, num_col_schro = 20000): # returns x,t
