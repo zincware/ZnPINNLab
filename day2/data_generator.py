3a58ec8283dbcdbbc023a4dc0aab1f992af697e8
Lorenzkraft <meyer_lorenz@web.de>
2023-11-18 16:23:36 +0100

    Removed old example and took norm of psi in p_loss

--- day2/data_generator.py
+++ day2/data_generator.py
@@ -28,47 +28,9 @@ class Schrodinger_Boundary(Dataset):
         return len(self.t)
     def getall(self):
         return self.x, self.t
-    
-class Schrodinger_Initial(Dataset):
-    def __init__(self, num_h_init = 50):            
-        h_func = lambda x : 2 * (1/torch.cosh(x))
-        # self.num_h_init = num_h_init
-        self.x = torch.tensor((lhs(1,samples=num_h_init)*10 - 5)).float().to(device)
-        self.t = torch.zeros((num_h_init, 1)).float().to(device)
-        self.h = torch.squeeze(torch.stack((
-            torch.tensor(h_func(self.x)).float().to(device),
-            torch.zeros((len(self.x),1)).to(device)
-        ),1))
-        return
-    def __getitem__(self,idx):
-        return self.x[idx], self.t[idx], self.h[idx]
-    def __len__(self):
-        return len(self.x), len(self.t), len(self.h)
-    def getall(self):
-        return  self.x, self.t, self.h
-    
-class Schrodinger_Initial_Oscillator_omega2(Dataset):
-    def __init__(self, num_h_init=50, n=1):
-        self.num_samples = num_h_init
-        factorial = lambda n: 1 if n == 0 else n * factorial(n - 1)
-        normfactor = lambda n: (1/torch.sqrt(torch.ones_like(self.x)*factorial(n)*2**n))*(2**(1/4)/torch.pi**(1/4))
-        psi = lambda x: normfactor(n)*torch.exp((-2*x**2) / 2) * special.hermite(n, monic=True)(torch.sqrt(2*torch.ones_like(x)).cpu()*x.cpu()).to(device)
-        self.x = torch.tensor((lhs(1,samples=num_h_init)*10 - 5)).float().to(device)
-        self.t = torch.zeros((num_h_init, 1)).float().to(device)
-        self.psi = torch.squeeze(torch.stack((
-            torch.tensor(psi(self.x)).float().to(device),
-            torch.zeros((len(self.x), 1)).to(device)
-        ),1))
-    def __getitem__(self, idx):
-        return self.x[idx], self.t[idx], self.psi[idx]
-    
-    def __len__(self):
-        return len(self.x)
-    
-    def getall(self):
-        return self.x, self.t, self.psi
+      
 
-class Schrodinger_Initial_Oscillator(Dataset):
+class Schrodinger_Initial(Dataset):
     def __init__(self, num_h_init=50, n=0):
         self.num_samples = num_h_init
         factorial = lambda n: 1 if n == 0 else n * factorial(n - 1)
@@ -95,7 +57,6 @@ class Schrodinger(Dataset):
         self.num_col_schro = num_col_schro
         self.x = torch.tensor(lhs(1,samples=num_col_schro)*10 - 5).float().to(device)
         self.t = torch.tensor(lhs(1,samples=num_col_schro)*(math.pi*4/5)).float().to(device)
-        #self.X = torch.squeeze(torch.dstack((x, t))).float()
         return 
     def __getitem__(self,idx):
         return self.x[idx], self.t[idx]
